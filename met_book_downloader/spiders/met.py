69ba3c1b89566ce2ff2ee081a7091ec11681e13d
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-02-05 13:12:26 -0500

    Updated met.py.

--- met_book_downloader/spiders/met.py
+++ met_book_downloader/spiders/met.py
@@ -18,7 +18,7 @@ class MetSpider(CrawlSpider):
 
         Rule(LinkExtractor(restrict_xpaths= [
             "//*[@class='metpubs-result']//*[@class='metpubs-result-button-container']"
-        ]), callback='parse_peliculas', follow=True)
+        ]), callback='parse', follow=True)
     )
 
     def parse_peliculas(self, response):
