602b0aac136b7675fe37bca0dbba35c1fac9ae75
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-02-05 12:59:46 -0500

    Updated met.py.

--- met_book_downloader/spiders/met.py
+++ met_book_downloader/spiders/met.py
@@ -30,8 +30,8 @@ class MetSpider(Spider):
         pdf_files = response.xpath('//a[@id="m_download_pdf_link"]/@onclick').extract()
         path = re.findall('\(\'(.*)\'\)', str(pdf_files), re.DOTALL)
 
-        # item_file = ItemLoader(BookItem(), response = response)
-        # item_file.add_value('file_urls', str(path))
-        # item = item_file.load_item()
-        # yield(item)
+        item_file = ItemLoader(BookItem(), response = response)
+        item_file.add_value('file_urls', str(path))
+        item = item_file.load_item()
+        yield(item)
         yield {"pewpewpewpewpwe" : path}
