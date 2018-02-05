7018e5db71126abd494e9aaf9e8cbbbee9868bd5
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-02-05 12:41:50 -0500

    Updated pipelines.py.

--- met_book_downloader/pipelines.py
+++ met_book_downloader/pipelines.py
@@ -5,6 +5,12 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+import os
+
+from scrapy.pipelines.files import FilesPipeline
+from scrapy.exceptions import DropItem
+
+
 
 class MetBookDownloaderPipeline(object):
     def process_item(self, item, spider):
