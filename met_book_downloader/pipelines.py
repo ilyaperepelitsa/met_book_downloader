ba30a66ac87978545b5ccf1d5684391b44d33084
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-02-05 12:42:47 -0500

    Updated pipelines.py.

--- met_book_downloader/pipelines.py
+++ met_book_downloader/pipelines.py
@@ -15,7 +15,7 @@ file_dir = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__
 class MetBookDownloaderPipeline(object):
     def process_item(self, item, spider):
         file_url = item['file_urls'][0]
-        filename = file_url.split("/")[-1].replace("-", " ").replace("_", " ")
+        filename = file_url.split("/")[-1].replace("_", " ").replace("_", " ")
         cur_file = item['files'][0]['path'].split('/')[-1]
         file_path = os.path.join(file_dir, cur_file)
         os.rename(file_path, os.path.join(file_dir, filename))
