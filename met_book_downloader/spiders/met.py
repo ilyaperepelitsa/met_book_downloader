5ac25fb30a69dfd100d4862da198d374b3971788
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-02-05 12:54:44 -0500

    Updated met.py.

--- met_book_downloader/spiders/met.py
+++ met_book_downloader/spiders/met.py
@@ -18,7 +18,7 @@ class MetSpider(Spider):
 
         Rule(LinkExtractor(restrict_xpaths= [
             "//*[@class='metpubs-result']//*[@class='metpubs-result-button-container']"
-        ]), callback='parse_book', follow=True),
+        ]), callback='parse', follow=True),
     )
 
     def parse_book(self, response):
