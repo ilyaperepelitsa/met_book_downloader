a2daa3a5012811f5c7a51a9b0f9a71e6da0448d1
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-02-05 12:52:27 -0500

    Updated items.py.

--- met_book_downloader/items.py
+++ met_book_downloader/items.py
@@ -4,9 +4,9 @@
 #
 # See documentation in:
 # http://doc.scrapy.org/en/latest/topics/items.html
-
+from scrapy.item import Item, Field
 import scrapy
-# 
+#
 #
 # class MetBookDownloaderItem():
 #     # define the fields for your item here like:
