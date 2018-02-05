acfa5c44460f21bd7b935b9259520577edc362dc
Ilya Perepelitsa <ilyaperepelitsa@Ilyas-iMac.home>
2018-02-05 12:36:09 -0500

    Updated met.py.

--- met_book_downloader/spiders/met.py
+++ met_book_downloader/spiders/met.py
@@ -21,12 +21,12 @@ class MetSpider(CrawlSpider):
 
     def parse_book(self, response):
         article = ItemLoader(item = BookItem(), response = response)
-        article.add_value('url', response.url)
+        # article.add_value('url', response.url)
         # article.add_xpath("book_title", '//div[@class="metpubs-title-container"]/text()', Join())
         # article.add_xpath("book_author", '//div[@class="metpubs-author-subcontainer"]/text()', Join())
 
         pdf_files = response.xpath('//a[@id="m_download_pdf_link"]/@onclick').extract()
-
+        path 
         item = article.load_item()
         return i
 
